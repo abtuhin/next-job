ccba1395cb906a10b74099f61d5f19a5fbcc680d
abtuhin <tuhin9pro@gmail.com>
2023-02-20 01:32:25 +0100

    add experience levels in job meta

--- src/hooks/useGetJobs.ts
+++ src/hooks/useGetJobs.ts
@@ -16,21 +16,25 @@ export default function useGetJobs() {
       const locations = data.includes.Entry.filter((item: any) => item.sys.contentType.sys.id === "location");
       const jobTypes = data.includes.Entry.filter((item: any) => item.sys.contentType.sys.id === "jobType");
       const jobDepartments = data.includes.Entry.filter((item: any) => item.sys.contentType.sys.id === "jobDepartment");
+      const jobLevels = data.includes.Entry.filter((item: any) => item.sys.contentType.sys.id === "jobLevel");
 
       const formattedJobs = jobs.map((job: any) => {
         const locationId = job.fields.locations[0].sys.id;
         const jobId = job.fields.type.sys.id;
         const departmentId = job.fields.department.sys.id;
+        const levelId = job.fields.levels[0].sys.id;
 
         const jobLocation = locations.find((item: any) => item.sys.id === locationId)
         const jobType = jobTypes.find((item: any) => item.sys.id === jobId)
         const jobDepartment = jobDepartments.find((item: any) => item.sys.id === departmentId)
+        const jobLevel = jobLevels.find((item: any) => item.sys.id === levelId)
 
         return {
           ...job,
           jobLocation: jobLocation.fields,
           jobType: jobType.fields,
-          jobDepartment: jobDepartment.fields
+          jobDepartment: jobDepartment.fields,
+          jobLevel: jobLevel.fields
         }
       });
       setAllJobs(formattedJobs)
